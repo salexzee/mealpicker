2cb58cff9bbd14cd38389cd46d55ec8253705f69
salexzee <samwebb@codingtestsite.com>
2016-05-21 13:45:58 -0700

    more arrow functions & more consistancy with semicolons

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get('/api*', (req, res) => {
     }
   }
   // Run it
-  request(options, callback);
+  request(options, callback)
 
 })
 
@@ -78,7 +78,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', routes)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found')
   err.status = 404
   next(err)
@@ -89,7 +89,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500)
     res.render('error', {
       message: err.message,
@@ -100,7 +100,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500)
   res.render('error', {
     message: err.message,
